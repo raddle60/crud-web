56528bead6be9a3716ea6e62c31c4113eff470e4
raddle60@gmail.com <raddle60@gmail.com@3feda76f-0336-7c13-e168-4b7773552a42>
2013-03-03 07:52:11 +0000

    生成Dao

---
+++ trunk/crud-def-dao/src/main/java/com/raddle/crud/dao/CrudDefinitionDao.java
@@ -0,0 +1,13 @@
+package com.raddle.crud.dao;
+
+import com.raddle.crud.dao.annotation.SqlMapper;
+import com.raddle.crud.dao.toolgen.CrudDefinitionMapper;
+
+/**
+ * 类CrudDatasourceDao.java的实现描述：dao
+ * @author raddle60 2013-3-3 下午1:37:22
+ */
+@SqlMapper
+public interface CrudDefinitionDao extends CrudDefinitionMapper {
+
+}
