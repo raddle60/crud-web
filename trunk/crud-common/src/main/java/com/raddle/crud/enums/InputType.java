2503cc853ebe8bdd2865be5c934aa16c35daf2c7
raddle60@gmail.com <raddle60@gmail.com@3feda76f-0336-7c13-e168-4b7773552a42>
2013-03-06 15:17:28 +0000

    表单项管理

---
+++ trunk/crud-common/src/main/java/com/raddle/crud/enums/InputType.java
@@ -0,0 +1,9 @@
+package com.raddle.crud.enums;
+
+/**
+ * 类InputType.java的实现描述：输入类型
+ * @author raddle60 2013-3-6 下午10:34:35
+ */
+public enum InputType {
+    INPUT, TEXTAREA, CHECKBOX, RADIO, SELECT;
+}
---
+++ trunk/crud-common/src/main/java/com/raddle/crud/enums/ItemFkType.java
@@ -0,0 +1,12 @@
+package com.raddle.crud.enums;
+
+public enum ItemFkType {
+    /**
+     * 一般定义
+     */
+    DEF,
+    /**
+     * 列表定义
+     */
+    DEF_LIST;
+}
