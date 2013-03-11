116c87d3777045aa35b7a8600ce6f351bc3ea793
raddle60 <raddle60@gmail.com>
2013-03-11 15:18:44 +0000

    复制属性

---
+++ crud-common/src/main/java/com/raddle/crud/enums/ItemCopyType.java
@@ -0,0 +1,16 @@
+package com.raddle.crud.enums;
+
+public enum ItemCopyType {
+    /**
+     * 拷贝标题
+     */
+    TITLE,
+    /**
+     * 拷贝顺序号
+     */
+    ORDER,
+    /**
+     * 拷贝类型（表单类型和输入信息）
+     */
+    TYPE
+}
