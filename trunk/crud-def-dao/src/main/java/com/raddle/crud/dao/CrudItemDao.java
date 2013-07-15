98e4b5a859e42f828354d8b52b2a4dde9b2d2241
raddle60@gmail.com <raddle60@gmail.com@3feda76f-0336-7c13-e168-4b7773552a42>
2013-07-15 14:13:37 +0000

    自动排序

--- trunk/crud-def-dao/src/main/java/com/raddle/crud/dao/CrudItemDao.java
+++ trunk/crud-def-dao/src/main/java/com/raddle/crud/dao/CrudItemDao.java
@@ -1,5 +1,9 @@
 package com.raddle.crud.dao;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+
 import com.raddle.crud.dao.annotation.SqlMapper;
 import com.raddle.crud.dao.toolgen.CrudItemMapper;
 
@@ -9,5 +13,11 @@ import com.raddle.crud.dao.toolgen.CrudItemMapper;
  */
 @SqlMapper
 public interface CrudItemDao extends CrudItemMapper {
-
+    @Select({
+        "select max(ITEM_ORDER)",
+        "from CRUD_ITEM",
+        "where DELETED = 0 and CRUD_DEF_ID = #{defId} and FK_TYPE = #{fkType}"
+    })
+    @ResultType(Integer.class)
+    Integer selectMaxOrder(@Param("defId") Long defId, @Param("fkType") String fkType);
 }
