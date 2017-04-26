c3255f595839091e4197c175fd6812579fa3d2ed
raddle60 <raddle60@xxx.com>
2017-04-26 20:03:09 +0800

    使用c3p0做数据源

--- crud-biz/src/main/java/com/raddle/crud/biz/impl/CrudDatasourceManagerImpl.java
+++ crud-biz/src/main/java/com/raddle/crud/biz/impl/CrudDatasourceManagerImpl.java
@@ -1,5 +1,7 @@
 package com.raddle.crud.biz.impl;
 
+import java.beans.PropertyVetoException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,9 +9,10 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.PooledDataSource;
 import com.raddle.crud.biz.CrudDatasourceManager;
 import com.raddle.crud.dao.CrudDatasourceDao;
 import com.raddle.crud.model.toolgen.CrudDatasource;
@@ -22,7 +25,7 @@ import com.raddle.crud.model.toolgen.CrudDatasourceExample;
 @Service("crudDatasourceManager")
 public class CrudDatasourceManagerImpl implements CrudDatasourceManager {
 
-    private static ThreadLocal<Map<Long, DataSource>> datasourceThreadCache = new ThreadLocal<Map<Long, DataSource>>();
+    private static ThreadLocal<Map<Long, CachedDataSource>> datasourceThreadCache = new ThreadLocal<Map<Long, CachedDataSource>>();
 
     @Autowired
     private CrudDatasourceDao crudDatasourceDao;
@@ -30,21 +33,41 @@ public class CrudDatasourceManagerImpl implements CrudDatasourceManager {
     @Override
     public DataSource getDatasource(Long id) {
         if (datasourceThreadCache.get() == null) {
-            datasourceThreadCache.set(new HashMap<Long, DataSource>());
-        }
-        if (datasourceThreadCache.get().containsKey(id)) {
-            return datasourceThreadCache.get().get(id);
+            datasourceThreadCache.set(new HashMap<Long, CachedDataSource>());
         }
         CrudDatasource crudDatasource = crudDatasourceDao.selectByPrimaryKey(id);
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName(crudDatasource.getDirverClassName());
-        ds.setUrl(crudDatasource.getUrl());
-        ds.setUsername(crudDatasource.getUsername());
-        ds.setPassword(crudDatasource.getPassword());
-        datasourceThreadCache.get().put(id, ds);
-        return ds;
+        if (datasourceThreadCache.get().containsKey(id)) {
+            if (datasourceThreadCache.get().get(id).isSameDs(crudDatasource)) {
+                return datasourceThreadCache.get().get(id).dataSource;
+            } else {
+                // 关闭数据源
+                PooledDataSource dataSource = datasourceThreadCache.get().get(id).dataSource;
+                try {
+                    dataSource.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+                datasourceThreadCache.get().remove(id);
+            }
+        }
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        try {
+            cpds.setDriverClass(crudDatasource.getDirverClassName()); //loads the jdbc driver 
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        cpds.setJdbcUrl(crudDatasource.getUrl());
+        cpds.setUser(crudDatasource.getUsername());
+        cpds.setPassword(crudDatasource.getPassword()); // the settings below are optional -- c3p0 can work with defaults 
+        cpds.setMinPoolSize(1);
+        cpds.setAcquireIncrement(2);
+        cpds.setMaxPoolSize(10);
+        cpds.setMaxIdleTime(1800);
+        cpds.setIdleConnectionTestPeriod(300);
+        datasourceThreadCache.get().put(id, new CachedDataSource(crudDatasource, cpds));
+        return cpds;
     }
-    
+
     @Override
     public CrudDatasource getDatasource(String code, String envCode) {
         CrudDatasourceExample example = new CrudDatasourceExample();
@@ -56,10 +79,27 @@ public class CrudDatasourceManagerImpl implements CrudDatasourceManager {
         }
         return null;
     }
-    
+
     public static void clearThreadCache() {
         datasourceThreadCache.remove();
     }
 
+    private class CachedDataSource {
+        private CrudDatasource crudDatasource;
+        private PooledDataSource dataSource;
+
+        public CachedDataSource(CrudDatasource crudDatasource, PooledDataSource dataSource) {
+            this.crudDatasource = crudDatasource;
+            this.dataSource = dataSource;
+        }
+
+        public boolean isSameDs(CrudDatasource other) {
+            if (other == null) {
+                return false;
+            }
+            return crudDatasource.getDirverClassName().equals(other.getDirverClassName()) && crudDatasource.getUrl().equals(other.getUrl()) && crudDatasource.getUsername().equals(other.getUsername())
+                    && crudDatasource.getPassword().equals(other.getPassword());
+        }
+    }
 
 }
